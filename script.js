b8081d60c6e31470f172afd523d962cd1844e70e
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2023-09-29 21:26:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+//lets start by getting out ID's to set start, stop and reset buttons.
 let startBtn = document.getElementById('start');
 let stopBtn = document.getElementById('stop');
 let resetBtn = document.getElementById('reset');
- 
+
+//start by initializing 00 to the time units.
+
 let hour = 00;
 let minute = 00;
 let second = 00;
 let count = 00;
- 
+
+//the click startbtn triggers the function and sets timer to True
+
 startBtn.addEventListener('click', function () {
     timer = true;
     stopWatch();
 });
- 
+
+//the click stoptbtn triggers the function and sets timer to False and pauses the timer
+
 stopBtn.addEventListener('click', function () {
     timer = false;
 });
+
+//the click resetbtn triggers the function and sets the timing units to 00 again and updates the values
  
 resetBtn.addEventListener('click', function () {
     timer = false;
@@ -27,7 +36,12 @@ resetBtn.addEventListener('click', function () {
     document.getElementById('sec').innerHTML = "00";
     document.getElementById('count').innerHTML = "00";
 });
- 
+
+//the call of function stopwatch() above runs recursively to update the units accordingly
+//If count reaches 100, it resets to 0, and second is incremented.
+//If second reaches 60, it resets to 0, and minute is incremented.
+//If minute reaches 60, it resets to 0, and hour is incremented.
+
 function stopWatch() {
     if (timer) {
         count++;
@@ -73,6 +87,8 @@ function stopWatch() {
         document.getElementById('min').innerHTML = minStr;
         document.getElementById('sec').innerHTML = secStr;
         document.getElementById('count').innerHTML = countStr;
+     //the setTimeout function is used to call the stopWatch function again 
+     //after a 10ms delay if timer is still true, creating a continuous loop.
         setTimeout(stopWatch, 10);
     }
 }
